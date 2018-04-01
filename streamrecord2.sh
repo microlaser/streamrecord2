5c77e7c871e4f7f7fa56f93e4249d1bfe070ada1
microlaser <microlaser@gmail.com>
2018-04-01 00:18:32 -0400

    Add files via upload
    this is my simple streamripper.  You must install mplayer for it to work.  On Ubuntu or Debian try "sudo apt-get install mplayer"
    to run simply type "sh streamrecord2" at the terminal

---
+++ streamrecord2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Please enter the full stream url you wish to record beginning with http://"
+read url
+echo "How many minutes would you like to record?"
+read record
+echo "Please enter the full path and filename you wish to record"
+read filepath
+
+mplayer $url -ao pcm:file\=$filepath -vc dummy -vo null &
+taskpid=$!
+sleep `echo $record\m`
+kill $taskpid
+
